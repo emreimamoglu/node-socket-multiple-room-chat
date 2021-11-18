04104d2028761047461d19539f1de912bbe170fa
Emre <imamogluemree@gmail.com>
2021-11-18 14:39:09 +0300

    socket broadcast added

--- client/client.js
+++ client/client.js
@@ -6,7 +6,10 @@ const roomInput = document.getElementById("room-input");
 const form = document.getElementById("form");
 
 const socket = io("http://localhost:3000");
-
+socket.on("connect",()=>{})
+socket.on("receive-message",(message)=>{
+    displayMessage(message);
+})
 form.addEventListener("submit",e=>{
     e.preventDefault();
     const message = messageInput.value;
@@ -14,6 +17,7 @@ form.addEventListener("submit",e=>{
 
     if(message === "") return
     displayMessage(message);
+    socket.emit('send-message',message);
     messageInput.value = "";
 })
 
